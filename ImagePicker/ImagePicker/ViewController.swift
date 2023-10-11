b2ca2b1a22af4f80c2f8a38e2e2c8e3307528b41
Weili Y <yi@weiliy.net>
2023-10-12 03:12:09 +0800

    feat: show alert

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -15,10 +15,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func experiment(sender: UIButton) {
-        
         let nextController = UIImagePickerController()
         present(nextController, animated: true, completion: nil)
         
+    }
+    
+    @IBAction func showAlert() {
+        let controller = UIAlertController()
+        
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "ok", style: .default) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        
+        present(controller, animated: true, completion: nil)
     }
 
 
